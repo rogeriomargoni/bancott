6d589b54cf7f348255a270e108cdd3881a8666b5
Rogerio_Margoni <margoni@gmail.com>
2019-02-27 20:10:16 -0300

    Versão inicial

---
+++ src/main/java/br/com/targettrust/bancott/BancottApplication.java
@@ -0,0 +1,13 @@
+package br.com.targettrust.bancott;
+//teste
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancottApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancottApplication.class, args);
+	}
+
+}
